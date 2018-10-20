392f56c6fd1083d52f2bf8941e3450417a23ec0f
Maria Chapman <thunderstarry@gmail.com>
2018-10-20 22:04:09 +0100

    Add createuser endpoint, use username from config file to make request

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-var http = require("http");
-var api = require("./api")(process.argv[2]);
-var route = require("./router").route;
-var url = require("url");
+const fs = require("fs");
+const http = require("http");
+const route = require("./router").route;
+const url = require("url");
 
-var handle = {
+const PORT = 9191;
+let username = null;
+
+try {
+  username = JSON.parse(fs.readFileSync("config.json", {encoding: "utf8"})).username;
+} catch {
+  console.log(`config.json not found - please press the link button on your Hue bridge and open localhost:${PORT}/createuser in your browser`);
+}
+
+const api = require("./api")(process.argv[2], username);
+
+const handle = {
   "/": api.indexServe,
   "/getlights": api.getLights,
   "/setlight": api.setLight,
+  "/createuser": api.createUser,
   "/js/dancer.js": api.dancer,
   "/js/jquery.js": api.jQuery,
   "/js/index.js": api.indexJs,
@@ -15,10 +27,10 @@ var handle = {
 }
 
 function onRequest(req, res) {
-  var pathname = url.parse(req.url).pathname;
-  console.log("Request for " + pathname + " received");
+  const pathname = url.parse(req.url).pathname;
+  console.log(`Request for ${pathname} received`);
   route(handle, pathname, req, res);
 }
 
-http.createServer(onRequest).listen(9191);
-console.log("Server started on port 9191");
+http.createServer(onRequest).listen(PORT);
+console.log(`Server started on port ${PORT}`);
